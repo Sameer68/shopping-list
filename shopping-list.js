2e9196cea2e98e4342f798cf6404f3e8b1450914
Graeme Stuart <ggstuart@gmail.com>
2020-01-10 16:45:08 +0000

    replace the example with a simpler and perhaps better alternative.

--- shopping-list.js
+++ shopping-list.js
@@ -1,88 +1,77 @@
-(function() {
-
-  // our data
-  let shoppingList;
-
-  // Get some references to the DOM
-  let listElement = document.getElementById('shopping-list');
-  let itemInput = document.getElementById('new-item');
-  let submitButton = document.getElementById('submit');
+(() => {
+  let listElement = document.getElementById('shopping');
+  let addButton = document.getElementById('add');
   let clearButton = document.getElementById('clear');
-  let saveButton = document.getElementById('save');
-  let revertButton = document.getElementById('revert');
 
-  // We build the DOM for the shopping list by creating an element for each item
-  let renderList = (list) => {
-    while (listElement.firstChild) {
-      listElement.removeChild(listElement.firstChild);
-    }
-    list.forEach(renderItem);
-  };
-
-  // Each element in the list contains a span with the item as text and a delete button
-  let renderItem = (item, index) => {
-
-    // create the DOM elements <li><span>item</span><button>x</button></li>
-    let listItem = document.createElement('li');
-    let listText = document.createElement('span');
+  // Add an item to the list
+  function addItem(item) {
+    let itemElement = document.createElement('li');
+    itemElement.textContent = item;
     let deleteButton = document.createElement('button');
     deleteButton.textContent = 'x';
-    listText.textContent = `${item} `;
-    listItem.appendChild(listText);
-    listItem.appendChild(deleteButton);
-
-    // This is a closure, the index value is accessed from the enclosing scope
-    // It adds a unique event listener to each button
-    // so each button deletes the correct item
+    itemElement.appendChild(deleteButton);
     deleteButton.addEventListener('click', ev => {
-      shoppingList.splice(index, 1);
-      renderList(shoppingList);
+      listElement.removeChild(itemElement);
     });
-
-    // Append the new elements to the DOM
-    listElement.appendChild(listItem);
+    listElement.appendChild(itemElement);
   };
 
-  // take the value from our input
-  let newItem = (ev) => {
-    shoppingList = shoppingList.concat(itemInput.value.split(',')); // add the new value to the list
-    itemInput.value = null;            // clear the input ready for another value
-    renderList(shoppingList);          // rebuild the DOM from the new list
-  };
+  // populate the list from an array
+  function renderList(list) {
+    list.forEach(item => {
+      addItem(item);
+    });
+  }
 
-  // clear the list and rebuild the DOM
-  let clearList = () => {
-    shoppingList = [];
-    renderList(shoppingList);
-  };
-
-  // Get our data from local storage
-  let loadFromStorage = () => {
-    shoppingList = localStorage.getItem('shopping-list');
-    if(shoppingList) {
-      // data are stored as a comma-separated string so need to be split
-      shoppingList = shoppingList.split(',');
-    } else {
-      shoppingList = [];
+  // Clear the list
+  function clearList() {
+    while(listElement.firstChild) {
+      listElement.removeChild(listElement.firstChild);
     }
-    renderList(shoppingList);
-  };
+  }
 
-  // Save our data or clear from local storage if the list is empty
-  let saveToStorage = () => {
-    if(shoppingList.length) {
-      localStorage.setItem('shopping-list', shoppingList);
+  // Add button
+  addButton.addEventListener('click', ev => {
+    let InputElement = document.getElementById('new-item');
+    if(InputElement.value) {
+      InputElement.value.split(',').forEach(v => {
+        addItem(v);
+      });
+      InputElement.value = null;
+    }
+  });
+
+  // submit on enter
+  document.getElementById('new-item').addEventListener("keyup", ev => {
+    if (ev.keyCode === 13) {
+      addButton.click();
+    }
+  });
+
+  // Clear button
+  clearButton.addEventListener('click', ev => {
+    clearList();
+  });
+
+  // Saving data for later use
+  window.addEventListener('beforeunload', ev => {
+    let items = [...listElement.childNodes];
+    if(items.length) {
+      let list = items.map(item => {
+        return item.textContent.slice(0, -1);
+      });
+      localStorage.setItem('shopping-list', list);
     } else {
       localStorage.removeItem('shopping-list');
     }
-  };
+  });
 
-  // event listeners for main buttons
-  clearButton.addEventListener('click', clearList);
-  submitButton.addEventListener('click', newItem);
-  saveButton.addEventListener('click', saveToStorage);
-  revertButton.addEventListener('click', loadFromStorage);
+  // Loading data from local storage
+  window.addEventListener('DOMContentLoaded', ev => {
+    let shoppingList = localStorage.getItem('shopping-list');
+    if(shoppingList) {
+      renderList(shoppingList.split(','));
+    }
+  });
 
-  // initialise the list by loading in any data from local storage
-  loadFromStorage();
 })();
